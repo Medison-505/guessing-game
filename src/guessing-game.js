361d4f2c47303640fcc141e0e351ead0e74ac9d6
Alena Skobeleva <askobeleva@affmy.com>
2019-10-02 18:16:06 +0300

    init: done guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.minPoint = 0;
+        this.maxPoint = 0;
+        this.lastCandidate = 0;
+    }
 
     setRange(min, max) {
-
+        this.minPoint = min;
+        this.maxPoint = max;
     }
 
     guess() {
-
+        let candidate = Math.ceil((this.maxPoint + this.minPoint)/2);
+        this.lastCandidate = candidate;
+        return candidate;
     }
 
     lower() {
-
+        this.setRange(this.minPoint, this.lastCandidate);
     }
 
     greater() {
-
+        this.setRange(this.lastCandidate, this.maxPoint);
     }
 }
 
